a3953a43505931156a172c8cacdee5b4ba114529
wakande <koechallanhi@gmail.com>
2024-10-17 13:08:48 -0700

    updated commets

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-Minimum Operations
+Minimum Operations Module
+This script calculates the minimum number of operations
+needed to achieve exactly `n` 'H' characters 
+using a combination of copy and paste operations.
 """
+
 
 def minOperations(n):
     """
-    Calculates the minimum number of operations required to achieve exactly `n` 'H' characters.
+    Calculate the minimum number of operations to reach exactly `n` 'H' characters.
     
-    :param n: The target number of 'H' characters.
-    :return: The minimum number of operations needed, or 0 if n is less than or equal to 1.
+    :param n: The target number of characters.
+    :return: The minimum number of operations required, or 0 if n is less than or equal to 1.
     """
     if n <= 1:
         return 0
@@ -16,6 +20,7 @@ def minOperations(n):
     operations = 0
     divisor = 2
     
+    # Continuously divide n by its smallest prime factors to count operations
     while n > 1:
         while n % divisor == 0:
             operations += divisor
